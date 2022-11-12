6d281efffab34431d772ac22993fbe7f81d0ded2
Andrew Dutson <Dutsandrew@gmail.com>
2022-11-12 12:23:24 -0700

    added where's waldo maps & characters

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import './styles/App.css';
 
+// Initialize Firebase
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyD34yDJ04Py9lOKIJOm8G8m83nhzSiaqiA",
+  authDomain: "photo-tagging-app-f39ec.firebaseapp.com",
+  projectId: "photo-tagging-app-f39ec",
+  storageBucket: "photo-tagging-app-f39ec.appspot.com",
+  messagingSenderId: "722301117573",
+  appId: "1:722301117573:web:44652e9fdd2675a00cb5ec"
+};
+const app = initializeApp(firebaseConfig);
+
 function App() {
   return (
     <div className="App">
